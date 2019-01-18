b30daae45a68f94111bc4227eed4929f93cc3773
domobritton <domonicbritton@gmail.com>
2019-01-17 22:59:10 -0800

    Test form data

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -5,13 +5,19 @@ export default class MovieForm extends Component {
   state = {
     text: '',
   }
-  
+
   render() {
     const { submitForm } = this.props;
     const { text } = this.state;
     return (  
         <form data-testid='movie-form' onSubmit={() => submitForm({ text })}>
-            <input type='text' />
+        <label htmlFor='text'>
+            Text
+            <input 
+            type='text'
+            id='text'
+            onChange={e => this.setState({text: e.target.value})} />
+        </label>
             <button>Submit</button>
         </form>
     )
--- src/MovieForm.test.js
+++ src/MovieForm.test.js
@@ -7,10 +7,24 @@ afterEach(cleanup);
 const onSubmit = jest.fn();
 
 test('<MovieForm>', () => {
-    const { queryByTestId, getByText } = render(<MovieForm submitForm={onSubmit} />);
+    const { queryByTestId, getByText, getByLabelText } = render(<MovieForm submitForm={onSubmit} />);
     
     expect(queryByTestId('movie-form')).toBeTruthy();
-    fireEvent.click(getByText('Submit'));
+    
+    // might not work
+    // getByLabelText('Text').value = 'hello';
+    // fireEvent.change(getByLabelText('Text'));
 
+    // nicer way to do it. will work
+    fireEvent.change(getByLabelText('Text'), {
+        target: { value: 'hello' }
+    });
+
+    fireEvent.click(getByText('Submit'));
     expect(onSubmit).toHaveBeenCalledTimes(1);
+    expect(onSubmit).toHaveBeenLastCalledWith({
+        text: 'hello',
+    });
 });
+
+// dont test state, test UI (functions being called with the correct data)
