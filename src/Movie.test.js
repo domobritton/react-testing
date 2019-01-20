9a0604e2d516a7a4f92d173d405a4295dc1d278f
domobritton <domonicbritton@gmail.com>
2019-01-19 23:26:55 -0800

    test with mock error and mock data

---
+++ src/Movie.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import { MemoryRouter } from 'react-router-dom';
+import Movie from './Movie';
+
+// change afterEach to clear the console.error after its tested and before next test
+afterEach(() => {
+    cleanup();
+    console.error.mockClear();
+});
+
+console.error = jest.fn();
+// test if no props are passed into <Movie />
+test('<Movie />', () => {
+    render(<Movie />);
+    expect(console.error).toHaveBeenCalled();
+});
+
+const movie = {
+    id: 'hi',
+    title: 'level up',
+    poster_path: 'abcd.jpg'
+};
+
+test('<Movie /> with movie', () => {
+    render(
+        <MemoryRouter>
+            <Movie movie={movie} />
+        </MemoryRouter>,
+    );
+    expect(console.error).not.toHaveBeenCalled();
+});
